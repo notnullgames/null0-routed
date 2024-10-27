0e59038db91b3d0f046f4e9c7b9993a480f35c05
David Konsumer <konsumer@jetboystudio.com>
2024-10-26 17:50:07 -0700

    better prettier

--- tools/gen_shared_host.js
+++ tools/gen_shared_host.js
@@ -1,53 +1,53 @@
-import path from "path";
-import fs from "fs/promises";
-import YAML from "yaml";
-import { glob } from "glob";
+import path from 'path'
+import fs from 'fs/promises'
+import YAML from 'yaml'
+import { glob } from 'glob'
 
 // Type mapping from YAML types to C types
 const typeMap = {
-  i32: "int",
-  u32: "unsigned int",
-  f32: "float",
-  string: "char*",
-  bool: "bool",
-  void: "void",
-  Image: "unsigned int", // Image handle
-  Font: "unsigned int", // Font handle
-  Color: "Color",
-  Sound: "unsigned int",
-  "Vector*": "Vector*",
-  Rectangle: "Rectangle",
-  Dimensions: "Dimensions",
-  ImageFilter: "ImageFilter",
-  "SfxParams*": "SfxParams*",
-  SfxPresetType: "SfxPresetType",
-  SfxWaveType: "SfxWaveType",
-  Key: "Key",
-  GamepadButton: "GamepadButton",
-  MouseButton: "MouseButton",
-  "u32*": "unsigned int*",
-  bytes: "unsigned char*",
-};
+  Image: 'unsigned int',
+  Font: 'unsigned int',
+  Sound: 'unsigned int',
+  i32: 'int',
+  u32: 'unsigned int',
+  f32: 'float',
+  string: 'char*',
+  bool: 'bool',
+  void: 'void',
+  Color: 'Color',
+  'Vector*': 'Vector*', // array
+  Rectangle: 'Rectangle',
+  Dimensions: 'Dimensions',
+  ImageFilter: 'ImageFilter',
+  'SfxParams*': 'SfxParams*',
+  SfxPresetType: 'SfxPresetType',
+  SfxWaveType: 'SfxWaveType',
+  Key: 'Key',
+  GamepadButton: 'GamepadButton',
+  MouseButton: 'MouseButton',
+  'u32*': 'unsigned int*', // array
+  bytes: 'unsigned char*'
+}
 
 // Generate type getter function
 function generateTypeGetter(type) {
-  const baseName = type.replace("*", "").replace("unsigned ", "");
+  const baseName = type.replace('*', '').replace('unsigned ', '')
   return `${type} get_${baseName}_arg() {
     ${type}* ret = (${type}*)copy_bytes_from_cart(cart_shared_arg_offset, sizeof(${type}));
     cart_shared_arg_offset += sizeof(${type});
     printf("HOST get_${baseName}_arg: %p\\n", (void*)ret);
     return *ret;
-}\n`;
+}\n`
 }
 
 // Generate type setter function
 function generateTypeSetter(type) {
-  const baseName = type.replace("*", "").replace("unsigned ", "");
+  const baseName = type.replace('*', '').replace('unsigned ', '')
   return `void set_${baseName}_ret(${type} value) {
     copy_bytes_to_cart((void*)&value, cart_shared_ret_offset, sizeof(value));
     cart_shared_ret_offset += sizeof(value);
     printf("HOST set_${baseName}_ret: %p\\n", (void*)&value);
-}\n`;
+}\n`
 }
 
 async function generateCode(yamlFiles) {
@@ -128,69 +128,66 @@ unsigned int cart_shared_arg_offset = 0;
 unsigned int cart_shared_ret_offset = 0;
 
 // Type getters/setters
-`;
+`
 
   // Generate enum for operations
-  let opEnum = "typedef enum {\n";
-  let mockImpls = "";
-  let switchCases = "";
+  let opEnum = 'typedef enum {\n'
+  let mockImpls = ''
+  let switchCases = ''
 
   // Process each YAML file
   for (const file of yamlFiles) {
-    const content = YAML.parse(await fs.readFile(file, "utf8"));
+    const content = YAML.parse(await fs.readFile(file, 'utf8'))
 
     // Track unique types for getter/setter generation
-    const types = new Set();
+    const types = new Set()
 
     // Process each function in the YAML
     Object.entries(content).forEach(([funcName, func]) => {
-      const opName = `OP_${funcName.toUpperCase()}`;
-      opEnum += `  ${opName},\n`;
+      const opName = `OP_${funcName.toUpperCase()}`
+      opEnum += `  ${opName},\n`
 
       // Generate mock implementation
-      const args = func.args || {};
+      const args = func.args || {}
       const argList = Object.entries(args)
         .map(([name, type]) => `${typeMap[type]} ${name}`)
-        .join(", ");
+        .join(', ')
 
-      const returnType = typeMap[func.returns] || "void";
-      types.add(returnType);
+      const returnType = typeMap[func.returns] || 'void'
+      types.add(returnType)
 
-      Object.values(args).forEach((type) => types.add(typeMap[type]));
+      Object.values(args).forEach((type) => types.add(typeMap[type]))
 
-      mockImpls += `${returnType} null0_${funcName}(${argList}) {\n`;
-      if (returnType !== "void") {
-        if (returnType === "unsigned int") {
-          mockImpls += `  return 1; // Mock handle\n`;
+      mockImpls += `${returnType} null0_${funcName}(${argList}) {\n`
+      if (returnType !== 'void') {
+        if (returnType === 'unsigned int') {
+          mockImpls += `  return 1; // Mock handle\n`
         } else {
-          mockImpls += `  ${returnType} ret = {0};\n  return ret;\n`;
+          mockImpls += `  ${returnType} ret = {0};\n  return ret;\n`
         }
       }
-      mockImpls += `}\n\n`;
+      mockImpls += `}\n\n`
 
       // Generate switch case
-      switchCases += `    case ${opName}:\n`;
-      if (returnType !== "void") {
-        switchCases += `      set_${returnType.replace("unsigned ", "")}_ret(`;
+      switchCases += `    case ${opName}:\n`
+      if (returnType !== 'void') {
+        switchCases += `      set_${returnType.replace('unsigned ', '')}_ret(`
       }
-      switchCases += `null0_${funcName}(`;
+      switchCases += `null0_${funcName}(`
 
       const argCalls = Object.entries(args)
-        .map(
-          ([name, type]) =>
-            `get_${typeMap[type].replace("*", "").replace("unsigned ", "")}_arg()`,
-        )
-        .join(",\n        ");
+        .map(([name, type]) => `get_${typeMap[type].replace('*', '').replace('unsigned ', '')}_arg()`)
+        .join(',\n        ')
 
-      switchCases += argCalls + ")";
-      if (returnType !== "void") {
-        switchCases += ")";
+      switchCases += argCalls + ')'
+      if (returnType !== 'void') {
+        switchCases += ')'
       }
-      switchCases += ";\n      break;\n\n";
-    });
+      switchCases += ';\n      break;\n\n'
+    })
   }
 
-  opEnum += "} Op;\n\n";
+  opEnum += '} Op;\n\n'
 
   // Generate final switch function
   const switchFunc = `
@@ -203,10 +200,10 @@ ${switchCases}
     default:
       fprintf(stderr, "Unknown op: %d\\n", op);
   }
-}\n`;
+}\n`
 
-  return output + opEnum + mockImpls + switchFunc;
+  return output + opEnum + mockImpls + switchFunc
 }
 
-const code = await generateCode(await glob("api/*.yml"));
-await fs.writeFile("host/null0_host.c", code);
+const code = await generateCode(await glob('api/*.yml'))
+await fs.writeFile('host/null0_host.c', code)
